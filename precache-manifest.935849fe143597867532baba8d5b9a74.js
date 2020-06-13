2a587b07755200b23a9c3d516f00bd2ff00aed73
kolesnikova9292 <kolesnikova9292@mail.ru>
2020-06-13 18:29:14 +0300

    Updates

---
+++ precache-manifest.935849fe143597867532baba8d5b9a74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb3afb906024bb8b7cd93a745de1827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44920df9a35f8fe1cd26",
+    "url": "/static/css/main.15c29334.chunk.css"
+  },
+  {
+    "revision": "efff077b615bc6f18213",
+    "url": "/static/js/2.020d32d0.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.020d32d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44920df9a35f8fe1cd26",
+    "url": "/static/js/main.cb12a1e7.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
