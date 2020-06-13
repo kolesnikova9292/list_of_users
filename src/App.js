f637d91c0a07028b0b769e405090eec9f411aa90
kolesnikova9292 <kolesnikova9292@mail.ru>
2020-06-13 16:03:54 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter,
+  HashRouter,
+} from "react-router-dom";
 import { Login } from "./login";
 import { Table } from "./table";
 import { PrivateRoute } from "./PrivateRoute";
@@ -12,26 +18,28 @@ const App = (props) => {
   const { isAuthorized } = props;
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute
-            path="/table"
-            component={Table}
-            exact
-            isAuthorized={isAuthorized}
-            loginPath="/"
-          />
-          <Route path="/" render={(props) => <Login {...props} />} exact />
-          <Route
-            path="/"
-            component={isAuthorized === true ? Table : Login}
-            exact
-          />
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <HashRouter basename="/">
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoute
+              path="/table"
+              component={Table}
+              exact
+              isAuthorized={isAuthorized}
+              loginPath="/"
+            />
+            <Route path="/" render={(props) => <Login {...props} />} exact />
+            <Route
+              path="/"
+              component={isAuthorized === true ? Table : Login}
+              exact
+            />
+            <Redirect to="/" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </HashRouter>
   );
 };
 
